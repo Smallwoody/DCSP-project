03100f4f889ebf1a13573a03077c724d39598cf6
Smallwoody <wksmallwod@gmail.com>
2018-11-20 14:59:00 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>JustA! pothole</title>
+
+    <style>
+        .error {
+          color: #FF0000;
+        }
+    </style>
+    
+</head>
+
+<body>
+</body>
+</html>
