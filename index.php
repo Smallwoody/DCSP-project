c3d253fff61e23067e3b2d0e696c0885f52f459e
Kohler Smallwood <wksmallwood@gmail.com>
2018-11-26 20:24:57 -0600

    jesus

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,4 +15,22 @@
 
 <body>
 </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>JustA! pothole</title>
+
+    <style>
+        .error {
+          color: #FF0000;
+        }
+    </style>
+    
+</head>
+
+<body>
+</body>
+>>>>>>> parent of 49650c5... Add files via upload
 </html>
