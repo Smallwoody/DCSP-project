00a703d82afccf81e379df22d1f47aa51eda39c8
Seryn13 <45214433+Seryn13@users.noreply.github.com>
2018-11-26 18:57:53 -0600

    added comment

--- stylesheets.php
+++ stylesheets.php
@@ -20,7 +20,7 @@ else {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+// comment
 _end;
 
 }
